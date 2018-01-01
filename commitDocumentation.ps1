f5e0f0860d27d6b48eaaafb178429525355155b4
michael dudley <michael.dudley@improvingenterprises.com>
2018-01-01 17:14:38 -0600

    setting name and email

--- commitDocumentation.ps1
+++ commitDocumentation.ps1
@@ -1,5 +1,6 @@
 ﻿$branch      = $args[0]
-
+git config --global user.email "mirukenjs@gmail.com"
+git config --global user.name  "build.miruken.com"
 cd ..\miruken.github.io
 git add .
 git commit -m "CI Documentation Build"
