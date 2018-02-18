2420d06c8f33c13986258627a4dd235c7d90f6df
michael dudley <michael.dudley@improvingenterprises.com>
2018-02-18 13:53:32 -0600

    cleaning up config

--- doc/conf.py
+++ doc/conf.py
@@ -1,43 +1,17 @@
-project = u'Miruken'
-copyright = u'2017, Miruken'
-author = u'Craig Neuwirt, Michael Dudley'
+project   = u'Miruken'
+copyright = u'2014-2018, Miruken'
+author    = u'Craig Neuwirt, Michael Dudley'
 
-extensions = []
-sourcesuffix = '.rst'
-master_doc = 'index'
-version = u''
-release = u''
+sourcesuffix     = '.rst'
+master_doc       = 'index'
 exclude_patterns = ['**/README.rst']
-pygments_style = 'sphinx'
+pygments_style   = 'sphinx'
 
-html_theme = "sphinx_rtd_theme"
+html_theme      = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
 html_theme_options = {
     'display_version': True,
     'prev_next_buttons_location': 'both',
-    # Toc options
     'collapse_navigation': False,
     'sticky_navigation': True
 }
-
-todo_include_todos = False
-htmlhelp_basename = 'Miruken'
-
-latex_elements = {
-}
-
-latex_documents = [
-    (master_doc, 'Miruken.tex', u'Miruken Documentation',
-     u'Michael Dudley', 'manual'),
-]
-
-man_pages = [
-    (master_doc, 'miruken', u'Miruken Documentation',
-     [author], 1)
-]
-
-texinfo_documents = [
-    (master_doc, 'Miruken', u'Miruken Documentation',
-     author, 'Miruken', 'One line description of project.',
-     'Miscellaneous'),
-]
